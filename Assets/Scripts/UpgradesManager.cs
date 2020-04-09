e67fa4d08ea04799e3ef3ef50cdf52d2f62cec89
antoinusitos <a.gargasson@rubika-edu.com>
2020-04-09 16:00:12 +0200

    doing inventory

---
+++ Assets/Scripts/UpgradesManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradesManager : MonoBehaviour
+{
+    private static UpgradesManager myInstance = null;
+
+    private List<Upgrade> myUpgrades = null;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        myInstance = this;
+        LoadUpgrades();
+    }
+
+    public UpgradesManager GetInstance()
+    {
+        return myInstance;
+    }
+
+    private void LoadUpgrades()
+    {
+        myUpgrades = new List<Upgrade>();
+        myUpgrades.Add(new MomentumBooster());
+        myUpgrades.Add(new OverampedBiceps());
+    }
+}
